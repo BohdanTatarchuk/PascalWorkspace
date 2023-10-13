08674356ff8e91411a8f1d5bd11f2302dfa8f743
Bogdan <tatarchukbogdan80@gmail.com>
2023-10-13 20:18:42 +0200

    First commiit Schachbrett

---
+++ Schachbrettmuster.pas
@@ -0,0 +1,11 @@
+(* Schachbrettmuster                                    Bohdan Tatarchuk, 13-10-2023
+   --------
+   Ein Programm, das einen ganzzahligen Wert n einliest und für eine gültige 
+   Eingabe (Wertebereich 1 ≤ n ≤ 20) ein zweidimensionales n-mal-n Schachbrett-Muster mit 
+   abwechselnden Leerzeichen und Sternchen * ausgibt.
+   ---------------------------------------------------------------------------------------
+*)
+PROGRAM Schachbrett;
+BEGIN (* Schachbrett *)
+   
+END. (* Schachbrett *)
