4f98380bdcf2433aebc17b4759c9beddf51c5abd
Bogdan <tatarchukbogdan80@gmail.com>
2023-10-16 20:06:25 +0200

    Schachbrettmuster ist fertig.

--- Schachbrettmuster.pas
+++ Schachbrettmuster.pas
@@ -6,6 +6,33 @@
    ---------------------------------------------------------------------------------------
 *)
 PROGRAM Schachbrett;
-BEGIN (* Schachbrett *)
-   
-END. (* Schachbrett *)
+   VAR
+      i, j, input: INTEGER;
+      line: string;
+BEGIN 
+      Read(input);
+      line := '';
+
+      //von 1 bis 20 Überprüfung
+      IF (input <= 20) THEN BEGIN
+        IF (input >= 1) THEN BEGIN
+          //Schleife für die Zeilen
+          FOR i := 1 TO input DO BEGIN
+            //Schleife für die Spalten
+            FOR j := 1 TO input DO BEGIN
+              IF ((i + j) mod 2 = 0) THEN BEGIN
+                line := line + '*';
+              END ELSE BEGIN
+                line := line + ' ';
+              END; //IF GERADE ODER UNGERADE
+            END; //FOR 2
+            line := line + #13#10;
+          END; //FOR 1
+          Write(line);
+        END ELSE BEGIN 
+          WriteLn('Number is smaller then 1');
+        END;//IF GREATER THEN 1
+      END ELSE BEGIN 
+        WriteLn('Number is greater then 20');
+      END;//IF SMALLER THEN 20
+END. //Schachbrett
