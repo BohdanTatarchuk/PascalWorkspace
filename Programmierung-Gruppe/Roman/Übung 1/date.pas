8a574275f57ed1e1dbb253b70507bef9842d60eb
Bogdan <tatarchukbogdan80@gmail.com>
2023-10-22 21:17:32 +0200

    Date programme is finished

--- Programmierung-Gruppe/Roman/Übung 1/date.pas
+++ Programmierung-Gruppe/Roman/Übung 1/date.pas
@@ -1,15 +1,17 @@
-(* Date          Bohdan Tatarchuk, 19=10=2023
-   ========
+(* Date          Bohdan Tatarchuk, 19-10-2023
+   ====
    A program that requires the user to input a correct date (as three numbers denoting day, month
    and year) and then computes how many days have passed since the beginning of the year.
-   ============================================================
+   ==============================================================================================
 *)
 
 PROGRAM Date;
 
-  VAR
-    day, month, year, i, result: INTEGER;
-    n: array [1..12] of INTEGER;
+//Global variables
+VAR
+  day, month, year, i, a, output: INTEGER;
+  n: array [1..12] of INTEGER;
+  result: BOOLEAN;
 
 BEGIN //date
   WriteLn('Write the date: ');
@@ -28,28 +30,37 @@ BEGIN //date
   n[11] := 30;
   n[12] := 31;
 
-  IF (day < 0) THEN BEGIN
-    WriteLn('Error: date is negative');
-  END ELSE IF (month < 0) THEN BEGIN
-    WriteLn('Error: date is negative');
-  END ELSE IF (year < 0) THEN BEGIN
-    WriteLn('Error: date is negative');
+  output := 0;
+
+  IF ((day <= 0) or (month <= 0) or (year <= 0)) THEN BEGIN
+    WriteLn('Error: date is negative or 0');
   END ELSE BEGIN
-    IF (1 <= month) THEN BEGIN
-      IF (month <= 12) THEN BEGIN
-        REPEAT
-          i := i + 1;
-          IF (day > n[i]) THEN BEGIN
-            WriteLn('Error: date is out of months range'); // 12.06.2005 
-          END ELSE BEGIN
-            result := day + n[i-1];
-          END; (* IF *)
-        UNTIL (month = i); //REPEAT
+    IF (month <= 12) THEN BEGIN
+      REPEAT
+        i := i + 1;
+
+        IF ((day > n[i]) or (day <= 0)) THEN BEGIN
+          result := false; 
+        END ELSE BEGIN
+          result := true;
+        END; //Days check
+
+      UNTIL (month = i); //REPEAT
+
+      IF (result) THEN BEGIN
+        
+        FOR a := 1 TO month - 1 DO BEGIN
+          output := output + n[a]; 
+        END; //Sum of days since the beginning of the year
+      
+      Writeln(output + day);
+
       END ELSE BEGIN
-        Write('Error: month is not between 1 and 12');
-      END; //IF
+        Writeln('Error: incorrect number of days');
+      END; //Days check
+
     END ELSE BEGIN
-      WriteLn('Error: month is not between 1 and 12');
+      WriteLn('Error: month is greater than 12');
     END; //Month check
   END; //Negative check
 END. //Date
